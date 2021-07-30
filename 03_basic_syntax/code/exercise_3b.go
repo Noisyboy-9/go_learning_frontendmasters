c6cd54c9958080d794e455f5559259dd4a84e8e0
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-30 17:02:35 +0430

    Finished exercise 3b about getting input from terminal

---
+++ 03_basic_syntax/code/exercise_3b.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var city string
+	var cityLivingDuration int
+	var weatherIdea bool
+
+	fmt.Print("please input your name:\t")
+	fmt.Scan(&name)
+
+	fmt.Print("please input your city:\t")
+	fmt.Scan(&city)
+
+	fmt.Print("How many years have you been living in ", city, ":\t")
+	fmt.Scan(&cityLivingDuration)
+
+	fmt.Print("is the weather good there:\t")
+	fmt.Scan(&weatherIdea)
+
+	fmt.Printf(
+		"Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.\n",
+		name,
+		city,
+		cityLivingDuration,
+		weatherIdea,
+	)
+
+}
